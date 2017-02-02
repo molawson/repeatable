928cd4ad4e8f796ffecb396a17190b5cbe3dafa8
Mo Lawson <moklawson@gmail.com>
2017-02-02 13:37:07 -0800

    Update Code Climate test reporter
    Code Climate changed how they expect the test coverage reporter to be
    run, so this updates to the latest version in the Code Climate and
    Travis CI docs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+require 'simplecov'
+SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'repeatable'
 
-Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
+Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
