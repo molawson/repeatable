2ef87a325ad3fd99d76c22c543c240961d82047c
Daniel Ma <drailskid@yahoo.com>
2017-05-04 08:00:15 -0700

    fix: don't use `Numeric#negative?` because it's a recent addition

--- lib/repeatable/expression/weekday_in_month.rb
+++ lib/repeatable/expression/weekday_in_month.rb
@@ -23,17 +23,18 @@ module Repeatable
       end
 
       def week_in_month(date)
-        zero_indexed_day = if count.negative?
-                             last_day_of_month(date).day - date.day
+        negative_count = count < 0
+        zero_indexed_day = if negative_count
+                             last_date_of_month(date).day - date.day
                            else
                              date.day - 1
                            end
         zero_indexed_week = zero_indexed_day / 7
 
-        (count.negative? ? -1 : 1) * (zero_indexed_week + 1)
+        (negative_count ? -1 : 1) * (zero_indexed_week + 1)
       end
 
-      def last_day_of_month(date)
+      def last_date_of_month(date)
         next_month = date.next_month
         first_day_of_next_month = ::Date.new(next_month.year, next_month.month, 1)
         first_day_of_next_month.prev_day
