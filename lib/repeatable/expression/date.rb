5f51ee7dabfdf3274f90577bf2cf56b514bc28d5
Mo Lawson <moklawson@gmail.com>
2019-07-25 18:01:55 -0500

    Fix biweekly first_occurrence value in to_h output
    It was never intended that the `first_occurrence` would be part of the
    hash representation of a biweekly expression. For the most part, it
    wasn't but this failing test showed that we not only have a bug, but
    we've had a bug for a while in that this `to_h` output would previously
    (i.e. before setting `@first_occurrence` in `initialize`) have been
    different for this class before and after calling `include?`. Before
    that call, the ivar was not set, but after the call, it is set.
    
    I chose to go the route of establishing a convention where prepending
    any ivar with an underscore would effectively make it "private" as far
    as the `to_h` method is concerned. This works well and seems pretty
    clear.

--- lib/repeatable/expression/date.rb
+++ lib/repeatable/expression/date.rb
@@ -25,8 +25,11 @@ module Repeatable
       sig {returns(T::Hash[T.untyped, T.untyped])}
       def attributes
         instance_variables.each_with_object({}) do |name, hash|
-          key = name.to_s.gsub(/^@/, '').to_sym
-          hash[key] = normalize_attribute_value(instance_variable_get(name))
+          key = name.to_s.gsub(/^@/, '')
+
+          next if key.start_with?('_')
+
+          hash[key.to_sym] = normalize_attribute_value(instance_variable_get(name))
         end
       end
 
