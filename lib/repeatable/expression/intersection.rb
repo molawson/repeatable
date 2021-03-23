92f5f486dd02f2659fe4e5fdcea6170ae18c8a3a
Mo Lawson <mo@molawson.com>
2021-03-22 20:36:45 -0500

    Flatten nested Intersection expressions
    As is true with `Unions`, nested `Intersection` elements are equivalent
    to their flattened representation.  With this implementation, there's
    not a good way to prove that equivalence in tests.  But the set theory
    bears this out.
    
    This mirrors the implementation for `Union`, and I did consider bumping
    this implementation up to their shared parent, `Set`. But that feels a
    little misleading, especially if any other `Set` classes come around for
    which doesn't share this particular characteristic of `Union`s and
    `Intersection`s.  I don't know what that set would be, but I think it's
    worth holding off for now. If we need to copy this a third time, then we
    can find the right abstraction (:hat_tip: Sandi Metz).

--- lib/repeatable/expression/intersection.rb
+++ lib/repeatable/expression/intersection.rb
@@ -1,6 +1,11 @@
 module Repeatable
   module Expression
     class Intersection < Set
+      def initialize(*elements)
+        other_intersection, not_intersection = elements.partition { |e| e.is_a?(self.class) }
+        super(other_intersection.flat_map(&:elements) + not_intersection)
+      end
+
       def include?(date)
         elements.all? { |e| e.include?(date) }
       end
--- spec/repeatable/expression/intersection_spec.rb
+++ spec/repeatable/expression/intersection_spec.rb
@@ -10,6 +10,31 @@ module Repeatable
 
       it_behaves_like 'an expression'
 
+      describe '#initialize' do
+        context 'when there are no Intersection elements' do
+          subject { described_class.new(twenty_third) }
+
+          it 'returns all elements as is' do
+            expected_hash = { intersection: [{ day_in_month: { day: 23 } }] }
+            expect(subject.to_h).to eq(expected_hash)
+          end
+        end
+
+        context 'when there are Intersection elements' do
+          subject { described_class.new(twenty_third).intersection(oct_thru_dec) }
+
+          specify do
+            expected_hash = {
+              intersection: [
+                { day_in_month: { day: 23 } },
+                { range_in_year: { start_month: 10, end_month: 12 } }
+              ]
+            }
+            expect(subject.to_h).to eq(expected_hash)
+          end
+        end
+      end
+
       describe '#include?' do
         it 'only returns true for dates that match all expressions' do
           expect(subject.include?(::Date.new(2015, 9, 23))).to eq(false)
