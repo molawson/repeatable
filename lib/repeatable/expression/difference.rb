95a5643e92ad6ab96b3c2657173c15b89576e915
Mo Lawson <mo@molawson.com>
2021-03-23 12:18:42 -0500

    Turn on strict typing for Set expressions
    As with the Date expressions, this was pretty straightforward with one
    exception. Because of Sorbet's poor support for splat operators, the
    setup we have where Intersection and Union do some optimization in their
    initialize methods, the types are a little funky for Set#initialize. The
    way I have it now is the easiest to parse but may not reflect reality in
    all situations.  I'm going to circle back with some tests to try and
    exercise these boundaries. The big bummer is that defining things in the
    easiest way for Sorbet (take a single array argument) would be a
    breaking change.  But maybe we can figure something out.

--- lib/repeatable/expression/difference.rb
+++ lib/repeatable/expression/difference.rb
@@ -1,20 +1,26 @@
-# typed: true
+# typed: strict
 module Repeatable
   module Expression
     class Difference < Base
+      extend T::Sig
+
+      sig { params(included: Expression::Base, excluded: Expression::Base).void }
       def initialize(included:, excluded:)
         @included = included
         @excluded = excluded
       end
 
+      sig { params(date: ::Date).returns(T::Boolean) }
       def include?(date)
         included.include?(date) && !excluded.include?(date)
       end
 
+      sig { returns(T::Hash[Symbol, T::Hash[Symbol, T.untyped]]) }
       def to_h
         Hash[hash_key, {included: included.to_h, excluded: excluded.to_h}]
       end
 
+      sig { params(other: Object).returns(T::Boolean) }
       def ==(other)
         other.is_a?(self.class) &&
           included == other.included &&
@@ -23,7 +29,11 @@ module Repeatable
 
       protected
 
-      attr_reader :included, :excluded
+      sig { returns(Expression::Base) }
+      attr_reader :included
+
+      sig { returns(Expression::Base) }
+      attr_reader :excluded
     end
   end
 end
--- lib/repeatable/expression/union.rb
+++ lib/repeatable/expression/union.rb
@@ -1,12 +1,17 @@
-# typed: true
+# typed: strict
 module Repeatable
   module Expression
     class Union < Set
+      extend T::Sig
+
+      sig { params(elements: Expression::Base).void }
       def initialize(*elements)
         other_unions, not_unions = elements.partition { |e| e.is_a?(self.class) }
+        other_unions = T.cast(other_unions, T::Array[Expression::Union])
         super(other_unions.flat_map(&:elements) + not_unions)
       end
 
+      sig { params(date: ::Date).returns(T::Boolean) }
       def include?(date)
         elements.any? { |e| e.include?(date) }
       end
